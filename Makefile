b43a8f7fb8c07c611e5ff6040a2ef886245faeb8
JD Butler <jdbutler5@crimson.ua.edu>
2019-02-18 20:38:33 -0600

    Tested menu options + basic windows

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 make: test.c
+	chmod 644 test.c
 	gcc -Wall -std=c11 test.c -lmenu -lncurses
--- test.c
+++ test.c
@@ -3,18 +3,30 @@
 #include <ncurses.h>
 #include <menu.h>
 
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+
+//Array for storing menu options.
 char *choices[] = {
-	"Choice 1",
-	"Choice 2",
-	"Choice 3",
-	"Choice 4",
+	"Create",
+	"Search",
+	"Update",
+	"Delete",
 	"Exit",
+};
+
+//Array for storing menu option descriptions.
+char *choice_desc[] = {
+	"- Create a New IMDB Record",
+	"- Search Through the IMDB Database",
+	"- Update an IMDB Record",
+	"- Delete an IMDB Record",
+	"- Exit the Program"
 };
 
 int main(){
     ITEM **menu_items;
 	MENU **menu;
-	int n_choices;
+	int n_choices, n_choice_desc;
 	int c;
 	
 	initscr();
@@ -22,18 +34,23 @@ int main(){
 	noecho();
 	keypad(stdscr, TRUE);
 	
-	n_choices = 5;
+	n_choices = ARRAY_SIZE(choices);
+	n_choice_desc = ARRAY_SIZE(choice_desc);
 	menu_items = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
 	
 	for (int i = 0; i < n_choices; i++)
-		menu_items[i] = new_item(choices[i], choices[i]);
+		menu_items[i] = new_item(choices[i], choice_desc[i]);
+	
 	menu_items[n_choices] = (ITEM *)NULL;
 	
 	menu = new_menu((ITEM **)menu_items);
+	hline("ls", LINES - 4);
 	post_menu(menu);
 	refresh();
 	
-	while((c = getch()) != KEY_F(1))
+	bool exit_cond = false;
+	
+	while((c = getch()) != KEY_F(1) && exit_cond == false)
 	{
 		switch(c)
 		{
@@ -43,6 +60,15 @@ int main(){
 			case KEY_UP:
 				menu_driver(menu, REQ_UP_ITEM);
 					break;
+			case 10: /*Enter*/
+				
+				mvprintw(LINES - 3, 0, "Your choice was %s", item_name(current_item(menu)));
+				
+				if(item_name(current_item(menu)) == "Exit"){
+					exit_cond = true;
+				}
+				
+				break;
 		}
 		
 	}
